eed84bd8a4d33be5771321a9ffa697f78e2e5d78
Vita-CatLord <vitaliy_matkovskiy@mail.ru>
2026-06-09 19:57:29 +0400

    dev-checkstyle

--- src/main/java/ru/practicum/shareit/user/repository/UserRepository.java
+++ src/main/java/ru/practicum/shareit/user/repository/UserRepository.java
@@ -6,5 +6,5 @@ import ru.practicum.shareit.user.models.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findUserByEmail(String Email);
+    User findUserByEmail(String email);
 }
