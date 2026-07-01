6f2d2a30273371b8d422b7767c45f16ea3109140
Vita-CatLord <vitaliy_matkovskiy@mail.ru>
2026-07-01 14:38:52 +0400

    dev- lost import

--- gateway/src/main/java/ru/practicum/shareit/user/UserController.java
+++ gateway/src/main/java/ru/practicum/shareit/user/UserController.java
@@ -4,6 +4,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.user.dto.UserDto;
 
 @RequiredArgsConstructor
